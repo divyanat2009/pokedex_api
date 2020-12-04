23cc19eaa9cbe89aae5dc836ed9735c60c26672c
divyanat2009 <69719463+divyanat2009@users.noreply.github.com>
2020-12-04 14:06:52 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+app.use(morgan('dev'))
+app.use((req, res) => {
+    res.send('Hello, divya!')
+  })
+
+app.listen(8000, ()=>{
+    console.log(`Server is listening at http://localhost:8000`)
+})
